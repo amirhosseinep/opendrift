e56cac5e06e766a31d64dfe14fc9f7d210c75e74
= <=>
2024-07-12 14:57:07 -0400

    b

--- export_data2.py
+++ export_data2.py
@@ -23,19 +23,10 @@ with open('drift_output.txt', 'w') as f:
     # Header section
     f.write('# Drift simulation initiated [UTC]:\n')
     f.write('simDate\tsimTime\n')
-    f.write('2019-04-12\t13:33:46\tGSHHS\n')
-    f.write('# Model version:\n')
-    f.write('modelVersion\n')
-    f.write('2.50\n')
-    f.write('# Object class id & name:\n')
-    f.write('objectClassId\tobjectClassName\n')
-    f.write('26\tLIFE-RAFT-DB11\n')
-    f.write('# Life raft capacity & search object length:\n')
-    f.write('lifeRaftCapacity\tsearchObjectLength\n')
-    f.write('6\t1\n')
-    f.write('# Number of route points\n')
-    f.write('numRoutePoints\n')
-    f.write('1\n')
+    f.write(f'{dates[0].strftime("%Y-%m-%d")}\t{dates[0].strftime("%H:%M:%S")}\n')
+    f.write('# Total number of steps\n')
+    f.write('totalSteps\n')
+    f.write(f'{len(dates)}\n')
 
     # Loop through time steps
     for i, date in enumerate(dates):
@@ -44,7 +35,7 @@ with open('drift_output.txt', 'w') as f:
         f.write(f'{date.strftime("%Y-%m-%d")}\t{date.strftime("%H:%M:%S")}\n')
         f.write('# Time passed [min] & [timesteps], now seeded, seeded so far:\n')
         f.write('timePassed\tnStep\tnowSeeded\tnSeeded\n')
-        f.write(f'{i*300}\t{i+1}\t0\t5000\n')  # 300 is the time step in seconds
+        f.write(f'{i*300}\t{i+1}\t10\t{i*10+10}\n')  # 300 is the time step in seconds
 
         # Mean position
         mean_lon = lons[:, i].mean()
@@ -55,9 +46,9 @@ with open('drift_output.txt', 'w') as f:
 
         # Particle data
         f.write('# Particle data:\n')
-        f.write('id\tlon\tlat\tstate\tage\torientation\n')
+        f.write('nStep\tid\tlon\tlat\n')
         for j in range(lons.shape[0]):
-            f.write(f'{j+1}\t{lons[j, i]:.4f}\t{lats[j, i]:.4f}\t11\t0\t1\n')
+            f.write(f'{i+1}\t{j+1}\t{lons[j, i]:.4f}\t{lats[j, i]:.4f}\n')
 
 # Close the file
 nc_file.close()
